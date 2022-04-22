5a9aa9256d1d4af8ee1b5311ae15ed9b93b9ad24
Yuval Shafrir <10yuval10@gmail.com>
2022-04-22 12:02:19 +0300

    first css phase complete

--- src/cmps/email-preview.jsx
+++ src/cmps/email-preview.jsx
@@ -6,11 +6,15 @@ export const EmailPreview = ({ email }) => {
     return (
         // <Link className="clean-link" title="email details" to={`/email/${email.id}`}>
         // <>
-        <div className="email-preview clean-link flex">
-            <p>{email.emailAddress}</p>
-            <p>{email.emailContent}</p>
-            {/* <img src={`${email.avatarPic}.jpg`} alt="" /> */}
-            <img src={email.avatarPic} alt="" />
+        <div className="email-preview-container flex">
+            <div className="email-preview flex">
+                <img src={email.avatarPic} alt="" />
+                <div className="address-and-content flex">
+                    <div className="email-address">{email.emailAddress}</div>
+                    <div className="email-content">{email.emailContent}</div>
+                </div>
+                {/* <img src={`${email.avatarPic}.jpg`} alt="" /> */}
+            </div>
         </div>
         // </>
         // </Link>
--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,8 +1,4 @@
-import { getDefaultNormalizer } from '@testing-library/react'
 import { storageService } from './async.storage.js'
-
-
-// let md5 = require('md5')
 
 const STORAGE_KEY = 'emailDB'
 
@@ -11,36 +7,10 @@ export const emailService = {
     save,
     query,
 }
-// let str = 'elik@bigpanda.io'
-// str = str.toLowerCase()
-// console.log(md5(str));
-// console.log(md5('Ori89@gmail.com'));
-
-
-// async function getAvatar(email) {
-//     // console.log(email);
-//     // email = email.toLowerCase()
-//     // let img = md5(email)
-//     try {
-//         const res = (`https://www.gravatar.com/avatar/${img}`)
-//         // console.log(res);
-//         // const avatar = res.data
-//         // console.log(avatar);
-//         return res
-
-//     } catch (err) {
-//         console.log('Couldn\'t get avatar pic', err);
-//     }
-// }
 
 async function query() {
     try {
         var emails = await JSON.parse(localStorage.getItem(STORAGE_KEY)) || []
-        // emails.map(async mail => {
-        //     return mail.avatarPic = await getAvatar(mail.emailAddress)
-
-        // })
-        // console.log(emails);
         return emails
     } catch (err) {
         console.log('Can\'t load emails', err)
